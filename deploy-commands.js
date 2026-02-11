b284d557c91624ec9b4e61fac3224920b5d449c3
Eric Grändinger <grandinger@outlook.com>
2026-02-11 18:07:27 +0100

    Changes to dotenv to use snake case consistently.

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,8 +1,8 @@
 const { REST, Routes } = require('discord.js');
 // using dotenv to get variables
 const token = process.env.DISCORD_TOKEN;
-const guildId = process.env.GUILDID;
-const clientId = process.env.CLIENTID;
+const guildId = process.env.GUILD_ID;
+const clientId = process.env.CLIENT_ID;
 const fs = require('node:fs');
 const path = require('node:path');
 
