ea543d1a31b6c5b885374f1e8af102878ebe1804
Eric Grändinger <grandinger@outlook.com>
2026-02-14 13:50:30 +0100

    Implemented unsubscribe command

--- commands/manageSubscribe/unsubscribe.js
+++ commands/manageSubscribe/unsubscribe.js
@@ -1,18 +1,24 @@
-const fs = require('fs');
-const path = './subscribers.json';
-const { SlashCommandBuilder, userMention } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const { readJson, writeToJson } = require('../../utils/jsonUtils.js');
+const path = 'utils/test.json'; // TOOD make it work in multiple guilds
 module.exports = {
 	cooldown: 5,
 	data: new SlashCommandBuilder().setName('unsubscribe').setDescription('Stop receiving notifications'),
 	async execute(interaction) {
-		const username = interaction.user.username;
-		// IMPOIRANT remember to save the id not name
-		// TODO
-		// retrieve name
-		// check if the name is contained in json file
-		  // if not do nothing
-		  // if true remove the name from the jason-file
-		// send message to confirm
-		await interaction.reply(userMention(interaction.user.id));
+		const result = await readJson(path);
+		if (!result.exists || !result.data.includes(interaction.user.id)) {
+			message = 'You are already unsubscribed';
+		}
+		else if (result.data.includes(interaction.user.id)) {
+			const index = result.data.indexOf(interaction.user.id);
+			if (index > -1) {
+				result.data.splice(index, 1);
+			}
+			message = await writeToJson(path, result.data) ? 'You are now unsubscribed' : 'There was an error writing to the file';
+		}
+		return interaction.reply({
+			content: message,
+			flags: MessageFlags.Ephemeral,
+		});
 	},
 };
