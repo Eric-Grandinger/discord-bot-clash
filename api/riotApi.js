ca8161a471dec1a70cdb29fb94b0947492ccbc34
Eric Grändinger <grandinger@outlook.com>
2026-02-20 16:02:40 +0100

    slightly improved error handling for ApiGet

--- api/riotApi.js
+++ api/riotApi.js
@@ -7,28 +7,31 @@ const api = axios.create({
 	headers: { 'X-Riot-Token': apiKey },
 });
 // https://coderscratchpad.com/using-axios-with-async-await-modern-asynchronous-syntax/
-async function riotGet(key) {
+async function riotGet(endpoint) {
 	try {
-		const response = await api.get(key);
-		return { exists : true, data:response.data };
+		const response = await api.get(endpoint);
+		return { success: true, statusCode: null, data: response.data };
 	}
 	catch (error) {
 		// https://axios.rest/pages/advanced/error-handling
 		if (error.response) {
 			console.log(error.response.statusText);
-			return { exists: false, data: error.response.status };
+			return { success: false, statusCode:error.response.status, data: null };
 		}
 		else if (error.request) {
 			console.log(error.request);
-			return { exists: false, data: error.request };
+			return { success: false, statusCode:'NETWORK_ERROR', data: null };
 		}
-		console.error(error.message);
-		throw error;
+		console.error(error.message);;
+		return { success: false, statusCode:'UNKNOWN_ERROR', data: null };
 	}
 }
 async function getTournamentData() {
 	// TODO handel riot errors
-	return riotGet('/tournaments');
+	const result = await riotGet('/tournaments');
+	if (result.success) {
+		return result.data;
+	}
+	return null;
 }
-
 module.exports = { getTournamentData };
