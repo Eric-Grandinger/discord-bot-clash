afa33c7664d5dc16d1c8d98e70a46edd6febf72c
Eric Grändinger <grandinger@outlook.com>
2026-03-06 09:51:30 +0100

    Flatten schedule array before storing schedules

--- scheduled/cacheRiotData.js
+++ scheduled/cacheRiotData.js
@@ -3,23 +3,21 @@ const { dbCacheTournamentData } = require('../database/db.js');
 
 async function cacheTurnamentData() {
 	const result = await getTournamentData();
-	let tournamentSchedule;
-	let tournamentData;
+	const tournaments = [];
+	const schedules = [];
 	if (!result.success) return;
-	// TODO check for change of structure from API
-	// if error check pritn error and return maybe crash?
-	// TODO maybe use {success: true/false, Data: data}
-	// This should only be printed in the DB file
-	for (let i = 0; i < result.data.length; i++) {
+	for (const item of result.data) {
 		try {
-			tournamentSchedule = result.data[i].schedule;
-			delete result.data[i].schedule;
-			tournamentData = result.data[i];
+			const { schedule, ...tournament } = item;
+			tournaments.push(tournament);
+			schedules.push(...schedule);
 		}
 		catch (error) {
 			console.log(error.message + ' In cacheTurnamentData');
 		}
-		await dbCacheTournamentData(tournamentData, tournamentSchedule);
 	}
+	console.log(tournaments);
+	console.log(schedules);
+	dbCacheTournamentData(tournaments, schedules);
 }
 module.exports = { cacheTurnamentData };
