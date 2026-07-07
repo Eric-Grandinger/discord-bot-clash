0f34141b0d2e2c8d4315508046a4926b890a9e53
a22erigr <a22erigr@student.his.se>
2026-07-07 13:05:34 +0200

    Updated database queris

--- database/db.js
+++ database/db.js
@@ -17,6 +17,10 @@ db.exec(schema, (err) => {
 	}
 });
 
+function dbCacheTournamentData(tournaments, schedules) {
+	cacheTournamentInfo(tournaments);
+	cacheTournamentSchedules(schedules);
+}
 function cacheTournamentInfo(tournaments) {
 	for (let index = 0; index < tournaments.length; index++) {
 		db.run('INSERT OR REPLACE INTO clashDataCache (id,themeId,nameKey) VALUES (?, ?, ?)', [
@@ -36,34 +40,49 @@ function cacheTournamentSchedules(schedules) {
 		]);
 	}
 }
-function dbCacheTournamentData(tournaments, schedules) {
-	cacheTournamentInfo(tournaments);
-	cacheTournamentSchedules(schedules);
-
+// https://github.com/TryGhost/node-sqlite3/wiki/Api#databaserunsql-param--callback
+async function tournamentExistsAsync(tournamentID) {
+	const result = db.get('SELECT exists (SELECT * FROM clashDataCache WHERE id = $tournamentID ) AS tournamentExists', { $tournamentID: tournamentID });
+	return result.tournamentExists;
 }
-function getTournamentData() {
+async function getTournamentData(tournamentID) {
 	// TODO what happens if no data exists
 }
-function signUpForTournament() {
+function signUpForTournament(userId, idOfClash) {
 
 }
-function optOutForTournament() {
+function optOutForTournament(userId, idOfClash) {
+	// should i get the latest or should it be accessed by id?
+}
+async function saveError(location, errorMsg) {
+	try {
+		await db.run('INSERT INTO errorInformation (location , errorMsg, time) VALUES ($location, $errorMsg, $time)',
+			{ $location: location,
+				$errorMsg: errorMsg.message ?? String(errorMsg),
+				$time: new Date().toLocaleString(),
+			});
+		console.log(location + ' ' + errorMsg.message);
+	}
+	catch (error) {
+		console.log('original error' + errorMsg + ' location ' + location);
+		console.log('New error ' + error);
+	}
+}
+
+function getSubscribedUser(userID, idOfClash) {
 
 }
-function isNotificationTimerSet() {
-// Return true/fakse
+async function getAllSubscribersAsync() {
+	// Return a list of all the subscribers who want to get updates
 }
-function getStartTime(error) {
-
-}
-function saveError(error) {
-
-}
-
-function getSubscribedUser(userID) {
-
-}
-function getAllSubscribers() 
-
-}
-module.exports = { dbCacheTournamentData, isNotificationTimerSet };
+module.exports = {
+	dbCacheTournamentData,
+	getTournamentData,
+	getStartTimeAsync,
+	getAllSubscribersAsync,
+	getSubscribedUser,
+	signUpForTournament,
+	optOutForTournament,
+	saveError,
+	tournamentExistsAsync,
+};
