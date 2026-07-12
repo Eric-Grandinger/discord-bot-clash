72bae009a72582465ae87b0929b34013f6255230
a22erigr <a22erigr@student.his.se>
2026-07-12 14:39:05 +0200

    Created/COPIED wrappers
    + The wrappers are from https://www.sqlitetutorial.net/sqlite-nodejs/
    ONT MY OWN

---
+++ database/dbWrappers.js
@@ -0,0 +1,40 @@
+// The wrappers are from https://www.sqlitetutorial.net/sqlite-nodejs/
+
+const execute = async (db, sql, params = []) => {
+	if (params && params.length > 0) {
+		return new Promise((resolve, reject) => {
+			db.run(sql, params, (err) => {
+				if (err) reject(err);
+				resolve();
+			});
+		});
+	}
+	return new Promise((resolve, reject) => {
+		db.exec(sql, (err) => {
+			if (err) reject(err);
+			resolve();
+		});
+	});
+};
+const fetchAll = async (db, sql, params) => {
+	return new Promise((resolve, reject) => {
+		db.all(sql, params, (err, rows) => {
+			if (err) reject(err);
+			resolve(rows);
+		});
+	});
+};
+
+const fetchFirst = async (db, sql, params) => {
+	return new Promise((resolve, reject) => {
+		db.get(sql, params, (err, row) => {
+			if (err) reject(err);
+			resolve(row);
+		});
+	});
+};
+module.exports = {
+	execute,
+	fetchAll,
+	fetchFirst,
+};
