1bc0c99441f9bca6f153a3c63994fdcbd705e412
a22erigr <a22erigr@student.his.se>
2026-07-15 22:10:55 +0200

    Both the data and schedule now share the same clash id

--- scheduled/cacheRiotData.js
+++ scheduled/cacheRiotData.js
@@ -1,5 +1,6 @@
 const { getTournamentData } = require('../api/riotApi.js');
 const { dbCacheTournamentData, tournamentExistsAsync, saveError } = require('../database/db.js');
+const { notifyNewClash } = require('../utils/messages.js');
 
 async function cacheTurnamentData() {
 	const result = await getTournamentData();
@@ -7,11 +8,14 @@ async function cacheTurnamentData() {
 	const schedules = [];
 	try {
 		if (!result.success) return;
-		if (!(await tournamentExistsAsync())) {
-		// TODO send notification of upcoming event
-		}
 		for (const item of result.data) {
 			const { schedule, ...tournament } = item;
+			for (const s of schedule) {
+				s.id = tournament.id;
+				if ((await tournamentExistsAsync(tournament.id))) { // IT should be !
+					notifyNewClash(tournament.id);
+				}
+			}
 			tournaments.push(tournament);
 			schedules.push(...schedule);
 		}
