204f52a4eb173e7a6eb077bde8e1ebd98b2230a7
a22erigr <a22erigr@student.his.se>
2026-07-15 22:09:36 +0200

    Implemented the logic for informing of new clash

--- utils/messages.js
+++ utils/messages.js
@@ -1,7 +1,9 @@
-const { Events, MessageFlags, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { Events, MessageFlags, ActionRowBuilder, ButtonBuilder, StringSelectMenuBuilder, ButtonStyle, ComponentType, EmbedBuilder } = require('discord.js');
 const channelId = process.env.DISCORD_CHANNEL_ID;
-let client;
+const { saveError, getAllSubscribersAsync, getTournamentData } = require('../database/db.js');
 
+let client;
+// Creating buttons https://www.mambahost.com/tools/discord-bot/button-builder/
 function init(_client) {
 	client = _client;
 }
@@ -18,3 +20,52 @@ async function notifyWeek() {
 async function notifyhour() {
 
 }
+async function notifyNewClash(idOfClash) {
+	try {
+		const users = await getAllSubscribersAsync();
+		const mentions = users.map(user => `<@${user.userId}>`).join(' ');
+
+		const clashData = await getTournamentData(idOfClash);
+		const startTime = new Date(clashData.startTime).toLocaleString();
+		const registrationTime = new Date(clashData.registrationTime).toLocaleString();
+
+		const row = new ActionRowBuilder().addComponents(
+			new ButtonBuilder()
+				.setCustomId('clash_signup , ' + idOfClash)
+				.setLabel('Sign up')
+				.setStyle(ButtonStyle.Primary),
+			new ButtonBuilder()
+				.setCustomId('clash_playersSigneup, ' + idOfClash)
+				.setLabel('Who are playing?')
+				.setStyle(ButtonStyle.Secondary),
+			new ButtonBuilder()
+				.setCustomId('clash_decline')
+				.setLabel('Can\'t make it')
+				.setStyle(ButtonStyle.Danger),
+		);// Secondary
+		const embed = new EmbedBuilder()
+			.setColor(0x5865f2)
+			.addFields(
+				{ name: 'Lock in starts', value: registrationTime, inline: true },
+				{ name: 'Lock in ends', value: startTime, inline: true },
+				{ name: 'Notifying', value: mentions },
+			)
+			.setTitle(clashData.nameKey)
+			.setImage('https://oyster.ignimgs.com/wordpress/stg.ign.com/2018/05/LOL-Clash.png')
+		;
+		const channel = await client.channels.fetch(channelId);
+		await channel.send({
+			components: [row],
+			embeds:[embed],
+		});
+	}
+	catch (error) {
+		saveError('In scheduler.js notifyNewClash', error);
+	}
+}
+module.exports = {
+	init,
+	notifyWeek,
+	notifyhour,
+	notifyNewClash,
+};
