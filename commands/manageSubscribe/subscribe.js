728765f93453c45a8df94b72b99a4c66c8c2e12b
a22erigr <a22erigr@student.his.se>
2026-07-22 18:11:36 +0200

    jason to using database

--- commands/manageSubscribe/subscribe.js
+++ commands/manageSubscribe/subscribe.js
@@ -1,13 +1,5 @@
 const { SlashCommandBuilder, MessageFlags } = require('discord.js');
-const { readJson, writeToJson } = require('../../utils/jsonUtils.js');
 const { signeUpForNotifications, isUserSignedUpForNotifications } = require('../../database/db.js');
-const path = 'utils/test.json'; // TOOD make it work in multiple guilds
-async function write(data) {
-	const result = await writeToJson(path, data);
-	return result.success
-		? 'you are now subscribed'
-		: 'There was an error writing to the file';
-}
 module.exports = {
 	cooldown: 5,
 	data: new SlashCommandBuilder().setName('subscribe').setDescription('Subscribe to get notified about upcoming events'),
@@ -20,21 +12,9 @@ module.exports = {
 			await signeUpForNotifications(interaction.user.id);
 			message = 'you are now subscribed';
 		}
-		/* const result = await readJson(path);
-
-		if (!result.success) {
-			message = await write([interaction.user.id]);
-		}
-		else if (result.data.includes(interaction.user.id)) {
-			message = 'you are already subscribed';
-		}
-		else {
-			result.data.push(interaction.user.id);
-			message = await write(result.data);
-		}*/
 		return interaction.reply({
 			content: message,
-			flags: MessageFlags.Ephemeral,
+			flags: MessageFlags.Ephemeral, // Only the user whom sent can see this response
 		});
 	},
 };
